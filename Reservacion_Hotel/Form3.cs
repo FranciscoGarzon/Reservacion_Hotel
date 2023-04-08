1815777474823cb4f736556580ba274198d2a5a7
David Almonacid <david.almonacidf@outlook.com>
2023-04-08 12:58:01 -0500

    Modificado formulario de ingreso del cliente

--- Reservacion_Hotel/Form3.cs
+++ Reservacion_Hotel/Form3.cs
@@ -16,20 +16,5 @@ namespace Reservacion_Hotel
         {
             InitializeComponent();
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form3_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
--- Reservacion_Hotel/Form6.cs
+++ Reservacion_Hotel/Form6.cs
@@ -21,10 +21,5 @@ namespace Reservacion_Hotel
         {
 
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
