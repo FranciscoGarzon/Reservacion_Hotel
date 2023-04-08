f917a60d42f6ab90923578a43fe7ff6dda746fe4
Francisco <franciscogarzonmoreno@gmail.com>
2023-04-08 13:00:50 -0500

    Creacion formularios Administrador y recepnista

--- Reservacion_Hotel/Form6.cs
+++ Reservacion_Hotel/Form6.cs
@@ -12,6 +12,7 @@ namespace Reservacion_Hotel
 {
     public partial class Form6 : Form
     {
+        Form home = new Form1();
         public Form6()
         {
             InitializeComponent();
@@ -24,6 +25,30 @@ namespace Reservacion_Hotel
 
         private void label1_Click(object sender, EventArgs e)
         {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(comboBox1.Text);
+            if (comboBox1.Text == "Administrador")
+            {
+                this.Hide();
+                home.Hide();
+                var signup = new Form7();
+                signup.ShowDialog();
+            }else if (comboBox1.Text == "Clientes")
+            {
+                this.Hide();
+                home.Hide();
+                var signup = new Form3();
+                signup.ShowDialog();
+            }else if (comboBox1.Text == "Recepcionista")
+            {
+                this.Hide();
+                home.Hide();
+                var signup = new Form3();
+                signup.ShowDialog();
+            }
 
         }
     }
