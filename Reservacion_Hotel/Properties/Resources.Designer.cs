f973c018454fdb56733b158ddb3ceba939d279cc
David Almonacid <david.almonacidf@outlook.com>
2023-04-08 11:12:54 -0500

    Cambio en el tamaño de la imagen

--- Reservacion_Hotel/Properties/Resources.Designer.cs
+++ Reservacion_Hotel/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Reservacion_Hotel.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap hotel_principal {
+            get {
+                object obj = ResourceManager.GetObject("hotel-principal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap logo {
             get {
                 object obj = ResourceManager.GetObject("logo", resourceCulture);
